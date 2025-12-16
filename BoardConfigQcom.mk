1238e7cefdfd87711883d2dc638cdf98fe8ca0ba
Luoxue <luoxue520owo@qq.com>
2025-12-16 20:17:30 +0800

    qcom: enable gralloc4 for UM 4.9 platforms with kernel 4.19
    Added support for halls on UM 4.9 platforms with kernel 4.19.

--- BoardConfigQcom.mk
+++ BoardConfigQcom.mk
@@ -149,6 +149,13 @@ ifneq ($(filter $(UM_5_4_FAMILY) $(UM_5_10_FAMILY) $(UM_5_15_FAMILY),$(TARGET_BO
     SOONG_CONFIG_qtidisplay_gralloc4 := true
 endif
 
+# Enable Supported halls on UM 4.9 platforms with kernel 4.19 upgrade
+ifeq ($(TARGET_KERNEL_VERSION),4.19)
+ifneq ($(filter $(UM_4_9_FAMILY),$(TARGET_BOARD_PLATFORM)),)
+    SOONG_CONFIG_qtidisplay_gralloc4 := true
+endif
+endif
+
 # Select AR variant of A-HAL dependencies
 ifneq ($(filter $(UM_5_10_FAMILY) $(UM_5_15_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     TARGET_USES_QCOM_AUDIO_AR ?= true
